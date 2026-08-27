b01dbd3ba217ba3bc2f87f1f9d6c5c23b69c0031
Ermina Joy A. Obsequio <oerminajoy@gmail.com>
2026-08-27 21:05:40 +0800

    Fix entrypoint copy directory

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 set -e
 
-if [ ! -d /var/lib/mysql/mysql ]; then
-    mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql
+# Start MySQL service
+service mysql start
+
+# Import database.sql if present and database is empty
+if [ -f /var/www/html/DATABASE/database.sql ]; then
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS capstone_db;" || true
+    mysql -u root capstone_db < /var/www/html/DATABASE/database.sql || true
 fi
 
-mysqld_safe --datadir=/var/lib/mysql >/var/log/mysql-init.log 2>&1 &
+# Ensure correct permissions for Apache root
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
 
-until mysqladmin ping --silent; do
-    sleep 1
-done
-
-# The PHP app connects to 127.0.0.1 as root with no password.
-# Ubuntu's default auth_socket plugin only permits local socket logins.
-mysql -uroot <<'SQL'
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
-CREATE USER IF NOT EXISTS 'root'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY '';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'127.0.0.1' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-SQL
-
-if [ -f /var/www/html/Capstone_demo/DATABASE/database.sql ]; then
-    mysql -uroot < /var/www/html/Capstone_demo/DATABASE/database.sql || true
-fi
-
-mysqladmin -uroot shutdown
-
-exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+# Start Supervisor (Apache & MySQL background runner)
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
